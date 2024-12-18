a803c8b3dd14b257e72f82b94d05b429ff8a3d18
angiratiwari <angiratiwari1102@gmail.com>
2024-12-19 02:57:57 +0530

    UC-1 added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // UC 1: Find the Frequency of Words in a Sentence
+        String sentence = "To be or not to be";
+        String[] words = sentence.split(" ");
+        HashTable<String, Integer> hashTable = new HashTable<>(10);
+
+        for (String word : words) {
+            Integer count = hashTable.get(word);
+            if (count == null) {
+                count = 0;
+            }
+            hashTable.insert(word, count + 1);
+        }
+
+        // Display frequencies for UC 1
+        System.out.println("Word frequencies in sentence:");
+        for (String word : words) {
+            System.out.println(word + ": " + hashTable.get(word));
+        }
     }
 }
